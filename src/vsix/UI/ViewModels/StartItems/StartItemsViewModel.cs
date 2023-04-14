6fdae9a36429f173b84b352870cebccac292b34e
Yann Duran <yd@live.com.au>
2023-04-14 15:57:11 +1000

    refactor: replace 3 unnecessary properties with fields

--- src/vsix/UI/ViewModels/StartItems/StartItemsViewModel.cs
+++ src/vsix/UI/ViewModels/StartItems/StartItemsViewModel.cs
@@ -15,23 +15,21 @@ namespace StartPagePlus.UI.ViewModels.StartItems
         private const string CHANGELOG_URL = WEBSITE_URL + "/changelog";
         private List<StartItemViewModel> items = new();
 
+        private readonly IStartItemDataService _dataService;
+        private readonly IStartItemCommandService _commandService;
+        private readonly IVisualStudioService _visualStudioService;
+
         public StartItemsViewModel(IStartItemDataService dataService, IStartItemCommandService commandService, IVisualStudioService vsService) : base()
         {
-            DataService = dataService;
-            CommandService = commandService;
-            VisualStudioService = vsService;
+            _dataService = dataService;
+            _commandService = commandService;
+            _visualStudioService = vsService;
             Heading = HEADING;
             IsVisible = true;
 
             GetCommands();
             Refresh();
         }
-
-        internal IStartItemDataService DataService { get; }
-
-        internal IStartItemCommandService CommandService { get; }
-
-        public IVisualStudioService VisualStudioService { get; }
 
         public List<StartItemViewModel> Items
         {
@@ -40,21 +38,21 @@ namespace StartPagePlus.UI.ViewModels.StartItems
         }
 
         private void GetCommands()
-            => Commands = CommandService.GetCommands(OpenChangelog, OpenWebsite, OpenOptions);
+            => Commands = _commandService.GetCommands(OpenChangelog, OpenWebsite, OpenOptions);
 
         private void OpenChangelog()
-            => VisualStudioService.OpenWebPage(CHANGELOG_URL, true);
+            => _visualStudioService.OpenWebPage(CHANGELOG_URL, true);
 
         private void OpenWebsite()
-            => VisualStudioService.OpenWebPage(WEBSITE_URL, true);
+            => _visualStudioService.OpenWebPage(WEBSITE_URL, true);
 
         private void OpenOptions()
-            => VisualStudioService.ShowOptions<OptionsProvider.General>();
+            => _visualStudioService.ShowOptions<OptionsProvider.General>();
 
         private void Refresh()
         {
             Items.Clear();
-            Items = DataService.GetItems();
+            Items = _dataService.GetItems();
         }
     }
 }
