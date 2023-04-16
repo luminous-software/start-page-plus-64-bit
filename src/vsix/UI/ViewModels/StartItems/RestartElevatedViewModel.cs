5cef25a90d7a9ea79a86d4275d6c4eb924e12f38
Yann Duran <yd@live.com.au>
2023-04-16 15:16:10 +1000

    bug(restart): fix incorrect 'elevated' values

--- src/vsix/UI/ViewModels/StartItems/RestartElevatedViewModel.cs
+++ src/vsix/UI/ViewModels/StartItems/RestartElevatedViewModel.cs
@@ -11,11 +11,11 @@ namespace StartPagePlus.UI.ViewModels.StartItems
             Moniker = KnownMonikers.User;
             Name = "Restart Visual Studio (elevated)";
             Description = "Restart Visual Studio as administrator with one click";
-            ImageSize = 42;
-            Margin = "9,5,0,0";
+            ImageSize = 49; // 47; // 45; // 42;
+            Margin = "9,-2,-3,0"; //"9,-2,-4,0"; //"9,-2,0,0"; //"9,0,0,0"; //"11,0,0,0"; //"9,0,0,0"; //"9,5,0,0";
         }
 
         protected override void OnClick()
-            => ActionService.RestartVisualStudio(confirm: true, elevated: false);
+            => ActionService.RestartVisualStudio(confirm: true, elevated: true);
     }
 }
--- src/vsix/UI/ViewModels/StartItems/RestartNormalViewModel.cs
+++ src/vsix/UI/ViewModels/StartItems/RestartNormalViewModel.cs
@@ -11,11 +11,11 @@ namespace StartPagePlus.UI.ViewModels.StartItems
             Moniker = KnownMonikers.Restart;
             Name = "Restart Visual Studio";
             Description = "Restart Visual Studio with one click (VS is currently running elevated, VS will remain elevated)";
-            ImageSize = 34;
-            Margin = "11,5,0,0";
+            ImageSize = 40;// 34;
+            Margin = "7,5,0,0"; // "11,5,0,0";
         }
 
         protected override void OnClick()
-            => ActionService.RestartVisualStudio(confirm: true, elevated: true);
+            => ActionService.RestartVisualStudio(confirm: true, elevated: false);
     }
 }
